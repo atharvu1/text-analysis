2c48d3c4e0967b14ade719ad926fad0ae37223ba
atharu1 <atharvu1@gmail.com>
2020-05-24 20:47:41 +0530

    Adding text analysis server

---
+++ app.py
@@ -0,0 +1,61 @@
+# app.py
+import flask
+from flask import Flask, render_template, request
+from jinja2 import  Environment
+from jinja2.loaders import FileSystemLoader
+#import json
+import pickle
+from flask_cors import CORS
+import nltk
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tag import pos_tag
+import re
+import string
+app = Flask(__name__)
+CORS(app)
+
+model = pickle.load(open('Minor_project_ml_model.pickle', 'rb'))
+
+def remove_noise(tweet_tokens, stop_words = ()):
+
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
+        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
+# home route
+@app.route("/")
+def home():
+    #return render_template('index.html')
+    return 'Home page'
+
+# serving form web page
+@app.route( '/get_sentiment', methods=['POST'] )
+def analyse_text():
+    data = request.data.decode('UTF-8')
+    print("\nRequest : ", data)
+    q = remove_noise(word_tokenize(data))
+
+    prediction = model.classify(dict([token, True] for token in q))
+
+    print(prediction)
+    return prediction
+app.run(debug = True)
